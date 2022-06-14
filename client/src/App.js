5a30e65a4693c767aaf0c5f9daec482ebc1966eb
jeyofdev <jgregoire.pro@gmail.com>
2022-06-14 12:23:20 +0200

    fix hot-reload

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>It's docker time !!!!!!!!</p>
+                <p>It's docker !!!!!!!</p>
                 <a
                     className="App-link"
                     href="https://reactjs.org"
